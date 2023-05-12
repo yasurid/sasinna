633ba98a3bfe5d01006a7a83268ccd2302d57f9e
Danushika Y <danushikay@gmail.com>
2023-05-12 10:50:08 +0530

    init
    init with config. spatia install and config

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\View\View;
+
+use App\Models\User;
+
+class UserController extends Controller
+{
+    //
+    public function show($id)
+    {
+       dd( User::findOrFail($id));
+    }
+}
